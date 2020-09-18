3f5515c675dbea51d5706eb2efb0607a50592136
Steven Bong <stevenseal99@yahoo.com>
2020-09-17 21:08:08 -0700

    Changed the timeblocks element from HTML to JS file. Worked on the timeblocks template from 9-5PM including the if/else conditional statement for past,present,future

--- script.js
+++ script.js
@@ -1,25 +1,142 @@
+// Make the time blocks array
+var timePlanner = [
+    {
+        id: "9",
+        hour: "09:00",
+        time: "9",
+    }, {
+        id: "10",
+        hour: "10:00",
+        time: "10",
+    }, {
+        id: "11",
+        hour: "11:00",
+        time: "11",
+    }, {
+        id: "12",
+        hour: "12:00",
+        time: "12",
+    }, {
+        id: "13",
+        hour: "13:00",
+        time: "13",
+    }, {
+        id: "14",
+        hour: "14:00",
+        time: "14",
+    }, {
+        id: "15",
+        hour: "15:00",
+        time: "15",
+    },{
+        id: "16",
+        hour: "16:00",
+        time: "16",
+    }, {
+        id: "17",
+        hour: "17:00",
+        time: "17",
+    },
+]
+
 // Declare necessary variables
 var saveBtn = $(".saveBtn");
+var textArea = $("#textarea");
+var compareTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 // Set the current date
 function currentTime(){
     var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").append(currentTime);
-    console.log(currentTime);
 }
 
 // Create the timeblocks for the planner
+timePlanner.forEach(function(currentHour){
+    // Create row
+    var createRow = $("<form>");
+    createRow.addClass("row");
+    $(".container").append(createRow);
 
-// Do if/else conditional statement to show past, present, and future color blocks
+    // Create first column
+    var timeColumn = $("<div>");
+    timeColumn.text(`${currentHour.hour}`);
+    timeColumn.addClass("col-md-1 hour");
+
+    // Create second column
+    var descriptionColumn = $("<div>");
+    descriptionColumn.addClass("col-md-9 description");
+    var descriptionArea = $("<textarea>");
+    descriptionColumn.append(descriptionArea);
+    descriptionColumn.add("id");
+
+    // Create if/else conditional statement to show past, present, and future color blocks
+    if(currentHour.time < compareTime){
+        descriptionColumn.addClass("past");
+    } else if (currentHour.time === compareTime){
+        descriptionColumn.addClass("present");
+    } else if (currentHour.time > compareTime){
+        descriptionColumn.addClass("future");
+    }
+
+    // Create third column
+    var saveBtn = $("<i>");
+    var saveColumn = $("<button>");
+    saveColumn.addClass("cold-md-2 saveBtn");
+    saveBtn.addClass("far fa-save fa-lg");
+    saveColumn.append(saveBtn);
+
+    // Append all the variables to the main element
+    createRow.append(timeColumn, descriptionColumn, saveColumn);
+
+    // // Declare the necessary variables to create the timeblocks
+    // var createRow = $("<form>");
+    // var timeColumn = $("<div>");
+    // var descriptionColumn = $("<div>");
+    // var descriptionArea = $("<textarea>");
+    // var saveColumn = $("<button>");
+    // var saveBtn = $("<i>");
+
+    // // Give text content to the variables
+    // timeColumn.text(`${currentHour.hour}`);
+
+    // // Give each variables their own classes
+    // createRow.addClass("row");
+    // timeColumn.addClass("col-md-1 hour");
+    // descriptionColumn.addClass("col-md-9 description");
+    // saveColumn.addClass("cold-md-2 saveBtn");
+    // saveBtn.addClass("far fa-save fa-lg");
+    // descriptionColumn.addClass("id");
+
+    // // Append the variables
+    // $(".container").append(createRow);
+    // descriptionColumn.append(descriptionArea);
+    // saveColumn.append(saveBtn);
+    // createRow.append(timeColumn, descriptionColumn, saveColumn);
+
+    // // Give attributes to the variables
+    // // descriptionColumn.attr("id", currentHour.id);
+
+    // // Create if/else conditional statement to show past, present, and future color blocks
+    // if(currentHour.time < compareTime){
+    //     descriptionColumn.addClass("past");
+    // } else if (currentHour.time === compareTime){
+    //     descriptionColumn.addClass("present");
+    // } else if (currentHour.time > compareTime){
+    //     descriptionColumn.addClass("future");
+    // }
+
+    // Append the variables to the main body
+    // createRow.append(timeColumn, descriptionColumn, saveColumn);
+})
 
 // Saves data to local storage
 function saveToLocal(){
-    localStorage.setItem("", JSON.stringify());
+    localStorage.setItem("timePlanner", JSON.stringify(timePlanner));
 }
 
-// To see the data that stored in the local storage
+// To keep the data exist everytime the user refresh the page
 function loadData(){
-
+    localStorage.getItem("timePlanner", JSON.parse(timePlanner));
 }
 
 // Add event listener to the save button
