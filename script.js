615c53ca1535880f4857f4098760c477add6cf28
Steven Bong <stevenseal99@yahoo.com>
2020-09-17 17:39:59 -0700

    Modified the HTML file and followed the one from Aslan's session. Worked on the JS file to create the pseudo code in what to do to create the planner. Started in declaring couple variables and functions

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 // Declare necessary variables
-var currentDate = $("#currentDay");
+var saveBtn = $(".saveBtn");
 
-// Set the current date to the browswer
-currentDate.HTML = new Date();
+// Set the current date
+function currentTime(){
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#currentDay").append(currentTime);
+    console.log(currentTime);
+}
+
+// Create the timeblocks for the planner
+
+// Do if/else conditional statement to show past, present, and future color blocks
+
+// Saves data to local storage
+function saveToLocal(){
+    localStorage.setItem("", JSON.stringify());
+}
+
+// To see the data that stored in the local storage
+function loadData(){
+
+}
+
+// Add event listener to the save button
+saveBtn.on("click", function(event){
+    // To prevent the form to load by itself
+    event.preventDefault();
+
+})
+
+// Call the current time function
+currentTime();
